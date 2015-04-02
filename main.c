1fec343b99e25a1736baa176be80362e76833214
henyxia <henyxia@live.fr>
2015-04-02 16:48:07 +0200

    NOT Working version

--- main.c
+++ main.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	int ret;
 	pthread_t tUI;
-	//pthread_t tNFC;
+	pthread_t tNFC;
 	pthread_t tBUS;
-	//pthread_t tHVC;
+	pthread_t tHVC;
 	initUILog();
 	if(!initLog())
 	{
@@ -26,25 +26,25 @@ int main(void)
 	displayUI();
 	ret = pthread_create(&tUI, NULL, drawUI, NULL);
 	printx(DEBUG, MAIN, "UI Started ID %08x ret %d\n", tUI, ret);
-	printx(INFO, MAIN, "Initializing NFC");/*
+	printx(INFO, MAIN, "Initializing NFC");
 	if(!initNFC())
 	{
 		printx(ERROR, MAIN, "Unable to start the NFC interface\n");
 		return 2;
 	}
-	pthread_create(&tNFC, NULL, processNFC, NULL);*/
+	pthread_create(&tNFC, NULL, processNFC, NULL);
 	if(!initBus())
 	{
 		printx(ERROR, MAIN, "Unable to start the BUS interface\n");
 		return 3;
 	}
-	pthread_create(&tBUS, NULL, processBus, NULL);/*
+	pthread_create(&tBUS, NULL, processBus, NULL);
 	if(!initHVC())
 	{
 		printx(ERROR, MAIN, "Unable to start the HVC interface\n");
 		return 4;
 	}
-	pthread_create(&tHVC, NULL, processHVC, NULL);*/
+	pthread_create(&tHVC, NULL, processHVC, NULL);
 	pthread_join(tUI, NULL);
 	closeLog();
 	return 0;
